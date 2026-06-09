d85327aa6de46ce416db69b46ce100216324e07b
amir <a.m.asadi2001@gmail.com>
2026-06-09 12:38:32 +0330

    create registration form and initial clean up

--- AuthECClient/src/app/user/registration/registration.component.ts
+++ AuthECClient/src/app/user/registration/registration.component.ts
@@ -1,12 +1,22 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule} from "@angular/forms";
 
 @Component({
   selector: 'app-registration',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './registration.component.html',
   styles: ``
 })
 export class RegistrationComponent {
+  form = new FormGroup({
+    fullName: new FormControl(''),
+    email: new FormControl(''),
+    password: new FormControl(''),
+    confirmPassword: new FormControl(''),
+  });
 
+  onSubmit() {
+    console.log(this.form.value);
+  }
 }

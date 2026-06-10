2f957c8d3d05ce0fc69dcbcfe29a24985e2ff107
amir <a.m.asadi2001@gmail.com>
2026-06-10 21:41:54 +0330

    handle signup form(validation and functionality)

--- AuthECClient/src/app/user/registration/registration.component.ts
+++ AuthECClient/src/app/user/registration/registration.component.ts
@@ -1,22 +1,58 @@
-import { Component } from '@angular/core';
-import { FormControl, FormGroup, ReactiveFormsModule} from "@angular/forms";
+import {Component} from '@angular/core';
+import {AbstractControl, FormControl, FormGroup, ReactiveFormsModule, ValidatorFn, Validators} from "@angular/forms";
+import {CommonModule} from "@angular/common";
+import {AuthService} from "../../shared/services/auth.service";
+import {ToastrService} from "ngx-toastr";
 
 @Component({
-  selector: 'app-registration',
-  standalone: true,
-  imports: [ReactiveFormsModule],
+  selector: 'app-registration', standalone: true, imports: [ReactiveFormsModule, CommonModule],
   templateUrl: './registration.component.html',
   styles: ``
 })
 export class RegistrationComponent {
+  constructor(private service: AuthService, private toastr: ToastrService) {
+  }
+
+  passwordMatchValidator: ValidatorFn = (control: AbstractControl): null => {
+    const password = control.get('password');
+    const confirmPassword = control.get('confirmPassword');
+
+    if (password && confirmPassword && password.value != confirmPassword.value) confirmPassword.setErrors({passwordMismatch: true}); else confirmPassword?.setErrors(null);
+
+    return null;
+  }
+
   form = new FormGroup({
-    fullName: new FormControl(''),
-    email: new FormControl(''),
-    password: new FormControl(''),
+    fullName: new FormControl('', Validators.required),
+    email: new FormControl('', [Validators.required, Validators.email]),
+    password: new FormControl('', [Validators.required, Validators.minLength(6), Validators.pattern(/(?=.*[^a-zA-Z0-9 ])/)]),
     confirmPassword: new FormControl(''),
-  });
+  }, {validators: this.passwordMatchValidator});
+
+  getControl(name: string) {
+    return this.form.get(name);
+  }
+
+  isInvalid(name: string): boolean {
+    const control = this.getControl(name);
+    return !!(control?.invalid && control?.touched );
+  }
 
   onSubmit() {
-    console.log(this.form.value);
+    if (this.form.valid) {
+      this.service.CreateUser(this.form.value).subscribe({
+        next: (res: any) => {
+          if (res.succeeded) {
+            this.form.reset();
+            this.toastr.success("ایجاد حساب کاربری با موفقیت ایجاد شد.");
+          }
+        }, error: (err) => {
+          console.error(err)
+          this.toastr.error("اینجاد حساب کاربری با خطا مواجه شد!");
+        }
+      });
+    } else {
+      this.toastr.warning("لطفا تمام اطلاعات را وارد کنید.");
+    }
   }
 }

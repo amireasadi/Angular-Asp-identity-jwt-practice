e4f9a472f8e275063cfa91649f705fe304588e86
amir <a.m.asadi2001@gmail.com>
2026-06-10 21:40:48 +0330

    create CreateUser service

--- AuthECClient/src/app/shared/services/auth.service.ts
+++ AuthECClient/src/app/shared/services/auth.service.ts
@@ -1,9 +1,16 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
+import {HttpClient} from "@angular/common/http";
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
+  baseUrl = "http://localhost:5216/api";
 
-  constructor() { }
+  constructor(private http: HttpClient) {
+  }
+
+  CreateUser(formData: any) {
+    return this.http.post(this.baseUrl + '/signup', formData);
+  }
 }

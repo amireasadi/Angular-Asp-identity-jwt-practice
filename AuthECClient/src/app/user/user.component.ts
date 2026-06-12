b87473439abf5f0da0c8a75234653047c8a3f20a
amir <a.m.asadi2001@gmail.com>
2026-06-12 20:20:29 +0330

    add animation to form

--- AuthECClient/src/app/user/user.component.ts
+++ AuthECClient/src/app/user/user.component.ts
@@ -1,15 +1,32 @@
-import { Component } from '@angular/core';
-import {RegistrationComponent} from "./registration/registration.component";
-import {RouterOutlet} from "@angular/router";
+import {Component} from '@angular/core';
+import {ChildrenOutletContexts, RouterOutlet} from "@angular/router";
 import {NgOptimizedImage} from "@angular/common";
+import {animate, query, style, transition, trigger} from "@angular/animations";
 
 @Component({
   selector: 'app-user',
   standalone: true,
   imports: [RouterOutlet, NgOptimizedImage],
   templateUrl: './user.component.html',
-  styles: ``
+  styles: ``,
+  animations: [
+    trigger('routerFadeIn', [
+      transition('* <=> *', [
+        query(':enter', [
+            style({opacity: 0}),
+            animate('0.5s ease-in-out', style({opacity: 1}))
+          ],
+          {optional: true}
+        ),
+      ])
+    ])
+  ]
 })
 export class UserComponent {
+  constructor(private context: ChildrenOutletContexts) {
+  }
 
+  getRouteUrl() {
+    return this.context.getContext('primary')?.route?.url;
+  }
 }

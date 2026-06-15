62d98526b291ca436fc4b8dc34f2f43eeacb0cf1
amir <a.m.asadi2001@gmail.com>
2026-06-15 19:42:25 +0330

    Create userProfile endpoint

---
+++ AuthECAPI/AuthECAPI/Controllers/AccountEndpoints.cs
@@ -0,0 +1,25 @@
+﻿using System.Security.Claims;
+using AuthECAPI.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+
+namespace AuthECAPI.Controllers;
+
+public static class AccountEndpoints
+{
+
+  public static IEndpointRouteBuilder MapAccountEndpoints(this IEndpointRouteBuilder app)
+  {
+    app.MapGet("/UserProfile", [Authorize] async (ClaimsPrincipal user, UserManager<AppUser> userManger) =>
+    {
+      string userId = user.Claims.First(x => x.Type == "UserId").Value;
+      var userDetails = await userManger.FindByIdAsync(userId);
+      return Results.Ok(new AppUser()
+      {
+        FullName = userDetails?.FullName,
+        Email = userDetails?.Email,
+      });
+    });
+    return app;
+  }
+}

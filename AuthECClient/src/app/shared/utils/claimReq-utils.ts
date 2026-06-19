4a1a33f32c26b2478579bbae79e828df999605bb
amir <a.m.asadi2001@gmail.com>
2026-06-19 12:04:05 +0330

    move all claim requirements to a file

---
+++ AuthECClient/src/app/shared/utils/claimReq-utils.ts
@@ -0,0 +1,7 @@
+﻿export const ClaimReq = {
+  adminOnly: (c: any) => c.role === 'Admin',
+  adminOrTeacher: (c: any) => c.role === 'Admin' || c.role === 'Teacher',
+  hasLibraryId: (c: any) => 'LibraryId' in c,
+  femaleAndTeacher: (c: any) => c.Gender === 'Female' && c.role === 'Teacher',
+  femaleAndUnder10: (c: any) => c.Gender === 'Female' && parseInt(c.DOB) < 10,
+}

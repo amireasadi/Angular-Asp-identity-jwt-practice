c6c8c20b339aa792c42604791c0132bd1e38aac6
amir <a.m.asadi2001@gmail.com>
2026-06-19 12:06:16 +0330

    update interceptor to handle 401 and 403 error gracefully

--- AuthECClient/src/app/shared/auth.interceptor.ts
+++ AuthECClient/src/app/shared/auth.interceptor.ts
@@ -1,16 +1,34 @@
-import { HttpInterceptorFn } from '@angular/common/http';
+import {HttpInterceptorFn} from '@angular/common/http';
 import {AuthService} from "./services/auth.service";
 import {inject} from "@angular/core";
+import {tap} from "rxjs";
+import {Router} from "@angular/router";
+import {ToastrService} from "ngx-toastr";
 
 export const authInterceptor: HttpInterceptorFn = (req, next) => {
   const authService = inject(AuthService);
+  const router = inject(Router);
+  const toastr = inject(ToastrService);
 
-  if(authService.isLoggedIn())
-  {
+  if (authService.isLoggedIn()) {
     const clonedReq = req.clone({
       headers: req.headers.set('Authorization', `Bearer ${authService.getToken()}`)
     });
-    return next(clonedReq);
+    return next(clonedReq).pipe(
+      tap({
+        error: (err: any) => {
+          if (err.status == 401) {
+            authService.deleteToken();
+            setTimeout(() => {
+              toastr.error('نشست منقضی شده، دوباره وارد شوید.');
+            }, 1500)
+            router.navigateByUrl('/signin');
+          } else if (err.status == 403) {
+            toastr.error('شما دسترسی لازم را ندارید!');
+          }
+        }
+      })
+    );
   }
   return next(req);
 };

9ada42286a2864056e14b7cea0e3fe9424634cf3
amir <a.m.asadi2001@gmail.com>
2026-06-19 12:05:10 +0330

    update authGuard to redirect user to forbidden page when accessed to an unauthorized page

--- AuthECClient/src/app/shared/auth.guard.ts
+++ AuthECClient/src/app/shared/auth.guard.ts
@@ -5,9 +5,22 @@ import {AuthService} from "./services/auth.service";
 export const authGuard: CanActivateFn = (route, state) => {
   const authService = inject(AuthService);
   const router = inject(Router);
+
   if(authService.isLoggedIn())
+  {
+    const claimReq = route.data['ClaimReq'] as Function;
+    if(claimReq)
+    {
+      if(!claimReq(authService.getClaims()))
+      {
+        router.navigateByUrl('/forbidden');
+        return false;
+      }
+      return true;
+    }
     return true;
+  }
   else
-    router.navigateByUrl('/signin')
+    router.navigateByUrl('/signin');
   return false;
 };
